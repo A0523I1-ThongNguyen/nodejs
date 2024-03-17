ecfed2ee0701cbbcc368e6714076a77c2a4464bd
A0523I1-ThongNguyen <thongnguyen51323@gmail.com>
2024-03-17 23:15:39 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("node:http"); //  platform nodejs : mặc định node cung cấp nhiều thư viện đã viết sẵn trong platform này.
+
+const hostname = "127.0.0.1"; // localhost
+const port = 3000; // Cổng : nơi chạy dự án của chúng ta
+
+// để tạo 1 server website : http hỗ trợ method createServer
+const server = http.createServer((req, res) => {
+  // nhờ hàm createServer chúng ta có thể render ra nội dung của website
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello Ngan and Thong\n");
+});
+
+// hàm listen cho server chạy : truyền 2 tham số để server biết chay trên cổng(port) và domain(hostname)
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
